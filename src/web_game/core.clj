a95649fe99c7b7a82565508bff510506e30e87ac
Tobias Gärdebro <tubbeg@gmail.com>
2023-09-30 13:18:46 +0200

    The web application can now serve static pages. It took a while to figure how to specify each path. I also didn't realize that wrap-resource defaults to octet-stream if you do not specify content-type.

--- src/web_game/core.clj
+++ src/web_game/core.clj
@@ -4,43 +4,63 @@
     [org.httpkit.server :as hk-server] 
     [compojure.core :refer :all]
     [compojure.route :as route]
-    [ring.util.response :refer [file-response]] 
-    [ring.middleware.resource :as resource])
+    [ring.middleware.not-modified :refer [wrap-not-modified]]
+    [ring.middleware.content-type :refer [wrap-content-type]]
+    [ring.util.response :refer [file-response
+                                resource-response
+                                content-type]] 
+    [ring.middleware.resource :refer [wrap-resource]])
   (:import (java.util Date)))
-
-(defn app [req]
-  {:status  200
-   :headers {"Content-Type" "text/html"}
-   :body    "hello there :) :)"})
 
 (file-response "index.html" {:root "resources/public"})
 
 (defroutes app2
   (GET "/index" [] "<h1>Hello World</h1>")
-  (GET "/" [] (file-response  "index.html" {:root "resources/public"}))
+  (GET "/" []
+    (file-response 
+     "index.html" 
+     {:root "resources/public"}))
   (route/not-found "<h1>Page not found</h1>"))
+
+
+(defn handleEntry [req]
+  (println "received request on entry: " req)
+  (let [r (-> (resource-response "public/index.html")
+              (content-type "text/html")
+              )]
+    (println "r is : " r)
+    r))
+
+(defn handleIndex [req]
+  (println "received req" req) 
+   "<h1>Hello World</h1>")
 
 (defroutes webRoutes
-  (GET "/index" [] "<h1>Hello World</h1>")
-  (GET "/" [] (file-response  "index.html" )); {:root "resources/public"}))
+  (GET "/index" [] handleIndex)
+  (GET "/" [] handleEntry)
+  ;(route/files "/")
   (route/not-found "<h1>Page not found</h1>"))
 
-(defn addMiddleWare []
-  (resource/wrap-resource webRoutes "public"))
-
+(defn addMiddleWare [routes]
+  (-> routes
+      (wrap-resource "resources/public")
+      (wrap-resource "resources/public/assets")
+      ;(wrap-content-type "text/html")
+      (wrap-content-type)
+      (wrap-not-modified)))
 
 (def now (Date.))
 now
 
-
 (def serverApp
-  (addMiddleWare))
+  (addMiddleWare webRoutes))
 
-(defn start-my-server [app]
-  (hk-server/run-server app {:port 3000}))
+(defn start-my-server [app port]
+  (hk-server/run-server app {:port port}))
 
 (defn -main
   [& args] 
   (println "Init")
-  (start-my-server serverApp))
+  (start-my-server webRoutes 3000))
+
 
